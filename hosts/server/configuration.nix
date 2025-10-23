ff4bc1d33ee3acd850b595c6f63d19de9dda68b9
umokee <hituaev@gmail.com>
2025-10-23 20:18:06 +1000

    dbus_off

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -4,11 +4,6 @@
     ./hardware-configuration.nix
   ];
 
-  services.dbus = {
-    enable = true;
-    implementation = "broker";
-  };
-
   networking.useDHCP = lib.mkDefault true;
   system.stateVersion = "25.05";
 }
