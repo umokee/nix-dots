6ee52757c39a1d2cd9b0db66d5504d869b7cef39
umokee <hituaev@gmail.com>
2025-10-23 17:11:53 +1000

    fixs

--- modules/nixos/0-base/security.nix
+++ modules/nixos/0-base/security.nix
@@ -17,34 +17,6 @@ in
         Defaults timestamp_timeout=5
         Defaults lecture="never"
       '';
-
-      extraRules = lib.optionals helpers.isServer [
-        {
-          users = [ conf.username ];
-          commands = [
-            {
-              command = "/run/current-system/sw/bin/systemd-run";
-              options = [ "NOPASSWD" ];
-            }
-            {
-              command = "/run/current-system/sw/bin/nix-env";
-              options = [ "NOPASSWD" ];
-            }
-            {
-              command = "/run/current-system/sw/bin/nixos-rebuild";
-              options = [ "NOPASSWD" ];
-            }
-            {
-              command = "/nix/store/*-nixos-system-*/bin/switch-to-configuration";
-              options = [ "NOPASSWD" ];
-            }
-            {
-              command = "/run/current-system/bin/switch-to-configuration";
-              options = [ "NOPASSWD" ];
-            }
-          ];
-        }
-      ];
     };
 
     security.polkit.enable = true;
