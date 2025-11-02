b91e0abd613125caa0aeeef343e346497a836020
umokee <hituaev@gmail.com>
2025-11-03 03:38:59 +1000

    1

--- modules/home-manager/3-services/brightness.nix
+++ modules/home-manager/3-services/brightness.nix
@@ -11,15 +11,15 @@ in
   config = lib.mkIf enable {
     home.packages = [ pkgs.brightnessctl ];
 
-    systemd.user.services.brightness = {
-      wantedBy = [ "graphical-session.target" ];
-      #after = [ "graphical-session.target" ];
+    #systemd.user.services.#brightness = {
+    #  wantedBy = [ #"graphical-session.#target" ];
+    #  #after = [ #"graphical-session.#target" ];
 
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.brightnessctl}/bin/brightnessctl set 50%";
-        RemainAfterExit = true;
-      };
-    };
+    #  serviceConfig = {
+    #    Type = "oneshot";
+    #    ExecStart = "${pkgs.#brightnessctl}/bin/#brightnessctl set 50%";
+    #    RemainAfterExit = true;
+    #  };
+    #};
   };
 }
